69c300d3ea2a5ee4d6cbbccb9caf12f5ee958fa8
Liciaav <liciaavasc@gmail.com>
2024-11-21 10:19:20 -0500

    fixeed erros on auth.ts and auth-routes.ts

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -2,8 +2,28 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to log in');
+    }
+
+    const data = await response.json();
+    return data.token;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 
 
 export { login };
+
